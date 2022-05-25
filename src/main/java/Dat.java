a8dfd6c1857640e125132ef90e7e1a2159f15a26
damdat97 <dam.dat97@gmail.com>
2022-05-25 11:05:47 +0700

    Dat toString2

--- src/main/java/Dat.java
+++ src/main/java/Dat.java
@@ -1,3 +1,10 @@
 public class Dat {
     private String name = "thay Kien lon";
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Dat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
